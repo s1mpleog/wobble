e75c3cebde3e3d3bba0985fc9d7206fc755b8d4d
s1mpleog <mrzkk07@gmail.com>
2024-01-21 11:17:19 +0530

    test

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -14,7 +14,7 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }) {
   return (
-      <section className="flex sm:gap-20 gap-2 sm:justify-normal justify-center sm:items-start items-start">
+      <section className="flex gap-10">
         <aside className="w-[270px] sm:block hidden">
           <DesktopSidebar />
         </aside>
